382664cdd52f7a03a981dda9dc8cbbc30557ebfa
Artem <salnikov.a.d@gmail.com>
2018-07-13 20:20:22 +0300

    create model methods call and start

--- app/controllers/multiplications_controller.rb
+++ app/controllers/multiplications_controller.rb
@@ -1,17 +1,29 @@
 class MultiplicationsController < ApplicationController
+  before_action :find_multiplication, only: :show
+
   def create
     multiplication = Multiplication.new(multiplication_params)
 
     if multiplication.save
       render json: { success: { code: 200, message: 'success' } }, status: 200
     else
-      render json: { error: { code: 400, message: 'error' } }, status: 400
+      render json: { error: { code: 400, message: multiplication.errors.messages } }, status: 400
     end
+  end
+
+  def show
+    render json: { error: { code: 404, message: "Multiplication not found" } }, status: 404 if @multiplication.nil?
+
+    @multiplication.call
   end
 
   private
 
   def multiplication_params
     params.require(:multiplication).permit(:max_multiplicand, :max_multiplier)
+  end
+
+  def find_multiplication
+    @multiplication = Multiplication.find_by(id: params[:id])
   end
 end
--- app/models/multiplication.rb
+++ app/models/multiplication.rb
@@ -10,4 +10,23 @@ class Multiplication < ApplicationRecord
               greater_than: -11,
               less_than: 11
             }
+
+  def call
+    start
+  end
+
+  def start
+    multiplicand_range = max_multiplicand > 0 ? (0..max_multiplicand) : (max_multiplicand..0)
+    multiplier_range = max_multiplier > 0 ? (0..max_multiplier) : (max_multiplier..0)
+
+    sum = multiplicand_range.reduce(0) do |acc, multiplicand|
+      multiplier_range.each do |multiplier|
+        result = multiplicand * multiplier
+        acc = acc + result
+      end
+      acc
+    end
+
+    self.update(sum: sum)
+  end
 end
